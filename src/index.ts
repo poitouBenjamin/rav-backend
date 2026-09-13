f255f9f00959211f5125e25c6e812835f596604b
Benjamin POITOU <poitoubenjamin@gmail.com>
2026-09-13 10:06:06 +0200

    feat: configure security middlewares (helmet, cors, rate limiting)

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
 import "dotenv/config";
+import helmet from "helmet";
+import cors from "cors";
+import { rateLimit } from "express-rate-limit";
 import express, { type Express, type Request, type Response } from "express";
 
 const app: Express = express();
 
-app.use(express.json());
+app.use(express.json({ limit: "100kb" }));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  }),
+);
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 min
+  limit: 100, // 100 requests
+  legacyHeaders: false,
+});
+app.use(limiter);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
